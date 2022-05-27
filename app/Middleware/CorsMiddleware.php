012515cfbbdf6b40536150c7c5758170d3de5075
zhangkang <873853298@qq.com>
2022-05-27 10:41:33 +0800

    跨域问题

--- app/Middleware/CorsMiddleware.php
+++ app/Middleware/CorsMiddleware.php
@@ -31,7 +31,7 @@ class CorsMiddleware implements MiddlewareInterface
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
         $response = Context::get(ResponseInterface::class);
-        $response = $response->withHeader('Access-Control-Allow-Origin', '*')
+        $response = $response->withHeader('Access-Control-Allow-Origin', $request->getHeaderLine('Origin'))
             ->withHeader('Access-Control-Allow-Credentials', 'true')
             ->withHeader('Access-Control-Allow-Headers', 'DNT,Keep-Alive,User-Agent,Cache-Control,Content-Type,Authorization')
             ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, OPTIONS, DELETE');
